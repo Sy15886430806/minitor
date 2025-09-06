cda5a396bffa268595780cb5e79a20aff2d33efe
Sy15886430806 <Sy15886430806@163.com>
2025-09-06 16:05:05 +0800

    客户端注册

---
+++ monitor-server/src/main/java/com/example/service/impl/ClientServiceImpl.java
@@ -0,0 +1,79 @@
+package com.example.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.entity.dto.Client;
+import com.example.mapper.ClientMapper;
+import com.example.service.ClientService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.util.Date;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class ClientServiceImpl extends ServiceImpl<ClientMapper, Client> implements ClientService {
+
+    public String registerToken = this.generateNewToken();
+
+    private final Map<Integer, Client> clientIDCache = new ConcurrentHashMap<>();
+    private final Map<String, Client> clientTokenCache = new ConcurrentHashMap<>();
+
+    @PostConstruct
+    public void initClient() {
+        this.list().forEach(this::addClientCache);
+    }
+
+    @Override
+    public String registerToken() {
+        return registerToken;
+    }
+
+    @Override
+    public Client findClientById(int id) {
+        return clientIDCache.get(id);
+    }
+
+    @Override
+    public Client findClientByToken(String token) {
+        return clientTokenCache.get(token);
+    }
+
+    @Override
+    public boolean verifyAndRegister(String token) {
+        if (this.registerToken.equals(token)) {
+            int id = this.randomClientID();
+            Client client = new Client(id, "未命名主机", token, new Date());
+            if (this.save(client)) {
+                registerToken = this.generateNewToken();
+                this.addClientCache(client);
+                return true;
+            }
+        }
+
+
+        return false;
+    }
+
+    private void addClientCache(Client client) {
+        clientIDCache.put(client.getId(), client);
+        clientTokenCache.put(client.getToken(), client);
+    }
+
+    private int randomClientID() {
+        return new Random().nextInt(90000000) + 10000000;
+    }
+
+    private String generateNewToken() {
+        String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        SecureRandom random = new SecureRandom();
+        StringBuffer sb = new StringBuffer(24);
+        for (int i = 0; i < 24; i++)
+            sb.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        System.out.println(sb);
+        return sb.toString();
+    }
+}

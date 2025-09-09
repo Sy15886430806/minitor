092159ac51c3c980bc11bea26c32c8779085b74e
Sy15886430806 <Sy15886430806@163.com>
2025-09-09 12:26:47 +0800

    历史数据图形展示

--- monitor-server/src/main/java/com/example/service/ClientService.java
+++ monitor-server/src/main/java/com/example/service/ClientService.java
@@ -9,6 +9,7 @@ import com.example.entity.vo.request.RenameNodeVO;
 import com.example.entity.vo.request.RuntimeDetailVO;
 import com.example.entity.vo.response.ClientDetailsVO;
 import com.example.entity.vo.response.ClientPreviewVO;
+import com.example.entity.vo.response.RuntimeHistoryVO;
 
 import java.util.List;
 
@@ -23,4 +24,6 @@ public interface ClientService extends IService<Client> {
     void renameClient(RenameClientVO vo);
     void renameNode(RenameNodeVO vo);
     ClientDetailsVO clientDetails(int clientId);
+    RuntimeHistoryVO clientRuntimeDetailsHistory(int clientId);
+    RuntimeDetailVO clientRuntimeDetailsNow(int clientId);
 }
